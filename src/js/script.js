7519ed07d99c28630e99d18aea8f042fb37498ce
Dylin Moore <dmoore26@aoiths.org>
2026-04-27 13:37:09 -0400

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,12 @@
-mapboxgl.accessToken = ' ACCESS TOKEN ';
+let MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidXNlcm5vdGZvdW5kMiIsImEiOiJjbW55d3JienIwNjh5MnBwcmIwMjVkdjB6In0.aS1PXiKXNaEE4H2oaF6kqg';
 
-const map = new mapboxgl.Map({
-	container: 'map',
-	center: [-74.5, 40],
-	zoom: 9
-});	
+	mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+	const map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [-74.5, 40],
+		zoom: 9
+	});
+
+
 
